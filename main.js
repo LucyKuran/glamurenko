37423c8c93d5475911765b666e5abc42a82a9b69
LucyKuran <anastacias98@mail.ru>
2022-11-23 03:40:50 +0700

    adding comments

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(() => {
   const nav = $('#topNav > div');
   $(document).scroll((e) => {
     nav.toggleClass('scrolled', $(e.currentTarget).scrollTop() > 550);
-  });
+  }); //WHEN SCROLLED MORE THAN 550PX, THE CLASS GETS TOGGLED
 
-  $('.navbar-toggler').click(() => nav.addClass('scrolled'));
+  $('.navbar-toggler').click(() => nav.addClass('scrolled')); //CLICKING ON MOBILE ADDS A CLASS
 });
